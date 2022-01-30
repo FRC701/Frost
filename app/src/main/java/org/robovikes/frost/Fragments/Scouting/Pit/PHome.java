b46381b4e327d61625898826721168c224723d3a
Charlotte Rains <charlotter@vandenrobotics.com>
2022-01-30 13:59:26 -0800

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/org/robovikes/frost/Fragments/Scouting/Pit/PHome.java
+++ app/src/main/java/org/robovikes/frost/Fragments/Scouting/Pit/PHome.java
@@ -6,7 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import android.widget.Button;
+
+import org.robovikes.frost.R;
 import org.robovikes.frost.databinding.FragmentPitHomeBinding;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 public class PHome extends Fragment {
 
@@ -17,12 +23,25 @@ public class PHome extends Fragment {
         binding = FragmentPitHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        Button button = root.findViewById(R.id.startPit);
+
         return root;
+
+    button.setOnClickListener(new View.OnClickListener()){
+        @Override
+        public void onClick(View view) {
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.nav_pit_active);
+
+        }
+    });
     }
 
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
