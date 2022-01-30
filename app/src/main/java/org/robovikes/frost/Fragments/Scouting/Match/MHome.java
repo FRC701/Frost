9415dddc3958c369b8c60ed66256ef378b247646
Charlotte Rains <charlotter@vandenrobotics.com>
2022-01-30 10:37:43 -0800

    fixed spinner error in match

--- app/src/main/java/org/robovikes/frost/Fragments/Scouting/Match/MHome.java
+++ app/src/main/java/org/robovikes/frost/Fragments/Scouting/Match/MHome.java
@@ -6,12 +6,9 @@ import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -25,7 +22,7 @@ import org.robovikes.frost.databinding.FragmentMatchHomeBinding;
 
 public class MHome extends Fragment{
 
-    private Spinner teamSpinner;
+
     private FragmentMatchHomeBinding binding;
     private int match = 1;
 
@@ -33,7 +30,6 @@ public class MHome extends Fragment{
 
         binding = FragmentMatchHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        setupSpinners();
 
         Button button = root.findViewById(R.id.startMatch);
         Button plusMatch = root.findViewById(R.id.plusMatch);
@@ -82,27 +78,5 @@ public class MHome extends Fragment{
     public void onDestroyView(){
         super.onDestroyView();
         binding = null;
-
-        public void setupSpinners(){
-            View root = binding.getRoot();
-
-            teamSpinner = root.findViewById(R.id.teamSpinner);
-
-            ArrayAdapter<CharSequence> teamAdapter = ArrayAdapter.createFromResource(root.getContext(),R.array.teamSpinner,android.R.layout.simple_spinner_item);
-            teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            teamSpinner.setAdapter(teamAdapter);
-
-            teamSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                    String choice = parent.getItemAtPosition(position).toString();
-                    Toast.makeText(binding.getRoot().getContext(), choice, Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> adapterView) {
-
-                }
-            });
     }
 }
