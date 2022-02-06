ef44a448f363f5ee65bed5bf0f09ac9302569e3e
Charlotte Rains <charlotter@vandenrobotics.com>
2022-02-06 10:25:24 -0800

    besides database work - pit is complete
    ~i think~ (hopefully)

--- app/src/main/java/org/robovikes/frost/Fragments/Scouting/Pit/PHome.java
+++ app/src/main/java/org/robovikes/frost/Fragments/Scouting/Pit/PHome.java
@@ -1,6 +1,5 @@
 package org.robovikes.frost.Fragments.Scouting.Pit;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -9,8 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
 import org.robovikes.frost.R;
 import org.robovikes.frost.databinding.FragmentPitHomeBinding;
 import androidx.navigation.NavController;
@@ -31,6 +35,7 @@ public class PHome extends Fragment {
         View root = binding.getRoot();
 
         Button button = root.findViewById(R.id.startPit);
+        setUpSpinners();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,17 +53,41 @@ public class PHome extends Fragment {
                 BottomNavigationView scoutingBar = root.findViewById(R.id.scouting_bar);
                 NavigationUI.setupWithNavController(scoutingBar, navController);
             }
+
         }, 10);
 
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         return root;
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
         binding = null;
+
+
     }
 
+    public void setUpSpinners() {
+        View root = binding.getRoot();
+
+        teamSpinner = root.findViewById(R.id.teamSpinner);
+
+        ArrayAdapter<CharSequence> teamAdapter = ArrayAdapter.createFromResource(root.getContext(), R.array.teams, android.R.layout.simple_spinner_item);
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamSpinner.setAdapter(teamAdapter);
+
+        teamSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String choice = parent.getItemAtPosition(position).toString();
+                Toast.makeText(binding.getRoot().getContext(), choice, Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
 }
