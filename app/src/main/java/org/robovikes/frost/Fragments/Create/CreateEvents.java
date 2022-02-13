013364ed16d1b297e0eebf3bc5996f36b3d75cfa
fatima <fatimaa@vandenrobotics.com>
2022-02-13 14:32:40 -0800

    string resources, & create pages
    finished extracting string resources and i started working on create pages

--- app/src/main/java/org/robovikes/frost/Fragments/Create/CreateEvents.java
+++ app/src/main/java/org/robovikes/frost/Fragments/Create/CreateEvents.java
@@ -1,13 +1,22 @@
 package org.robovikes.frost.Fragments.Create;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.robovikes.frost.R;
 import org.robovikes.frost.databinding.FragmentCreateEventsBinding;
 
 public class CreateEvents extends Fragment {
@@ -18,6 +27,19 @@ public class CreateEvents extends Fragment {
 
         binding = FragmentCreateEventsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+                BottomNavigationView eventsTeamsBar = root.findViewById(R.id.create_bar);
+                NavigationUI.setupWithNavController(eventsTeamsBar, navController);
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+            }
+        }, 10);
 
         return root;
     }
